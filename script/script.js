38bf14080e3bae30bee922d692acc7cec59a7257
Cosmin <kosmyn1994@gmail.com>
2023-10-17 12:11:26 +0200

    benchmark file and style added

---
+++ script/script.js
@@ -0,0 +1,16 @@
+let totalSeconds = 60;
+
+function updateTimer() {
+  if (totalSeconds <= 0) {
+    clearInterval(timerId);
+    document.getElementById("time").textContent = "00";
+    return;
+  }
+
+  const seconds = String(totalSeconds).padStart(2, "0");
+  document.getElementById("time").textContent = seconds;
+  totalSeconds--;
+}
+
+const timerId = setInterval(updateTimer, 1000);
+updateTimer(60);
